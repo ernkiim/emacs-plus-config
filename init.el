b6c59c1cc262a6771796b38778eae24683c0be36
e <e@es-MacBook-Air.lan>
2025-05-23 01:05:32 -0400

    added winum bindings

--- init.el
+++ init.el
@@ -24,6 +24,7 @@
         use-package-compute-statistics t))
 
 ;; ---------- PATH injection ---------- ;;
+
 ;; Update here every time you change PATH
 (setenv "PATH" "/Users/e/.cabal/bin:/Users/e/.ghcup/bin:/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/bin:/System/Cryptexes/App/usr/bin:/usr/bin:/bin:/usr/sbin:/sbin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin:/Library/TeX/texbin:/Users/e/Library/Application Support/Coursier/bin:/opt/homebrew/bin:$HOME/.local/bin:$HOME/.ghcup/bin:$HOME/.ghcup/ghc/9.6.7/bin:$HOME/.cabal/bin:/Library/TeX/texbin:")
 (setq exec-path (split-string (getenv "PATH") path-separator))
@@ -33,6 +34,53 @@
 ;;   :init
 ;;   (when (memq window-system '(mac ns x))
 ;;   (exec-path-from-shell-initialize)))
+
+
+;; ---------- Preferences ---------- ;;
+
+(setq mouse-wheel-tilt-scroll t
+      mouse-wheel-flip-direction t)
+
+;; buffer management
+(keymap-global-set "M-o" 'other-window)
+(keymap-global-set "s-w" 'delete-window)
+(keymap-global-set "C-s-w" 'kill-buffer-and-window)
+
+;; Navigate windows with number keys
+(use-package winum
+  :bind (("s-0" . 'winum-select-window-0-or-10)
+         ("s-1" . 'winum-select-window-1)
+         ("s-2" . 'winum-select-window-2)
+         ("s-3" . 'winum-select-window-3)
+         ("s-4" . 'winum-select-window-4)
+         ("s-5" . 'winum-select-window-5)
+         ("s-6" . 'winum-select-window-6)
+         ("s-7" . 'winum-select-window-7)
+         ("s-8" . 'winum-select-window-8)
+         ("s-9" . 'winum-select-window-9))
+  :config
+  (winum-mode))
+
+
+;; Scroll zoom conflicts with smooth scroll momentum
+(keymap-global-unset "C-<wheel-up>")
+(keymap-global-unset "C-<wheel-down>")
+
+;; Save history
+(savehist-mode 1)
+
+;; Smooth Scroll
+(pixel-scroll-precision-mode 1)
+
+;; Mood-line is MUCH faster than doom
+(use-package mood-line
+  :init
+  (mood-line-mode t)
+  :custom
+  (mood-line-glyph-alist mood-line-glyphs-fira-code))
+
+;; Theme
+(use-package dracula-theme)
 
 
 ;; ---------- Marginalia, Vertico, Consult ---------- ;;
@@ -87,35 +135,6 @@
 ;;   )
 
 
-;; ---------- Preferences ---------- ;;
-
-(setq mouse-wheel-tilt-scroll t
-      mouse-wheel-flip-direction t)
-
-;; buffer management
-(keymap-global-set "s-w" 'delete-window)
-(keymap-global-set "C-s-w" 'kill-buffer-and-window)
-
-
-;; Scroll zoom conflicts with smooth scroll momentum
-(keymap-global-unset "C-<wheel-up>")
-(keymap-global-unset "C-<wheel-down>")
-
-;; Save history
-(savehist-mode 1)
-
-;; Smooth Scroll
-(pixel-scroll-precision-mode 1)
-
-;; Mood-line is MUCH faster than doom
-(use-package mood-line
-  :init
-  (mood-line-mode t)
-  :custom
-  (mood-line-glyph-alist mood-line-glyphs-fira-code))
-
-;; Theme
-(use-package dracula-theme)
 
 ;; ---------- Programming modes ---------- ;;
 
@@ -179,7 +198,8 @@
  '(package-selected-packages
    '(auctex consult dracula-theme exec-path-from-shell flycheck
             haskell-mode marginalia mood-line nerd-icons-completion
-            orderless pdf-tools sbt-mode scala-mode vertico vterm))
+            orderless pdf-tools sbt-mode scala-mode vertico vterm
+            winum))
  '(tool-bar-mode nil))
 
 (custom-set-faces
